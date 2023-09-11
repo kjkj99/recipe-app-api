4d7c67a21e3be7d9c8d57f2a9b029b44b8408f4f
K J <kjkjkjkj99.21@gmail.com>
2023-09-11 18:05:10 +0530

    cmt

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,13 +1,22 @@
+"""
+Sample tests
+"""
 from django.test import SimpleTestCase
+
 from app import calc
 
 
-class CalcTest(SimpleTestCase):
-    def test_add(self):
-        res = calc.add(2, 4)
-        self.assertEqual(res, 6)
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
 
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
 
-    def test_sub(self):
-        res = calc.sub(3, 1)
-        self.assertEqual(res, 2)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
