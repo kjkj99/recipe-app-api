aad84a1f6110d0f04a44e4eca9c2f0cc1bf95d37
K J <kjkjkjkj99.21@gmail.com>
2023-09-11 17:25:29 +0530

    commit

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+
+    def test_add(self):
+
+        res = calc.add(2,4)
+
+        self.assertEqual(res, 6)
+
+    def test_sub(self):
+
+        res = calc.sub(3,1)
+
+        self.assertEqual(res, 2)
